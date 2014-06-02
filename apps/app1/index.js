23888c2028d442509bb6a198f75f4eec11386259
Charles Lam <charles.lam@gmx.at>
2014-06-02 08:16:32 +0200

    Added IP-Remember Code

--- apps/app1/index.js
+++ apps/app1/index.js
@@ -4,34 +4,31 @@ var router = express.Router();
 var path  = __dirname.split("\\");
 var matchPath = "/" + path.slice(path.length-2).join("/");
 
-router.get(matchPath, function(req, res) {
-  //res.send('index -- 1 -- NEW ' + matchPath);
-  res.render("../apps/app1/views/index",{title:"SUPPPPerereeere"});
-});
-	 
-	
-module.exports = router;
 
-
-/*
-
-var http = require('http');
 var fs = require('fs');
 var ENCODING = "utf8";
-var FILENAME = "";
+var FILENAME = __dirname + "/caller-ip.txt";
 
-http.createServer(function (req, res) {
-  fs.readFile(FILENAME, ENCODING ,function(err, data){
-	if(err != null || data!=req.connection.remoteAddress) {
-		fs.writeFile(FILENAME, req.connection.remoteAddress, ENCODING, 
-		{flags: 'w'}, function(){
-			res.end("NEW");
-		});
-	}else{
-		res.end("OLD");
-	}
+router.get(matchPath, function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-  });
-}).listen(8080);
+	fs.readFile(FILENAME, ENCODING ,function(err, data){
+		if(err == null || data!="") {
+			res.end(data);
+		}
+	});
+});
 
-*/
+router.put(matchPath, function(req, res, next) {
+	var ipToSave = req.query.ip || req.connection.remoteAddress;
+	fs.writeFile(FILENAME, ipToSave, ENCODING, 
+	{flags: 'w'}, function(err){
+		if(err != null)
+			next(err);
+		else{
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end();
+		}
+	});
+});
+	 
+module.exports = router;

e2a1e6667f5ae659a4a02841f449ad7845bf057d
Charles Lam <charles.lam@gmx.at>
2014-06-02 15:38:14 +0200

    minor refactoring & optimisations

--- engine.js
+++ engine.js
@@ -5,82 +5,85 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-var  apps = {"/":routes, "/users":users};
-
-function init(){
-	var fs = require('fs');
-	var path = "./apps/";
-	
-	fs.readdir(path,function(){
-	var files = arguments[1];
-	for(var idx in files){
-		fs.stat(path + files[idx], function(filePath){
-			return function(err, stat){
-				if(stat.isDirectory()){
-					apps[filePath.replace(/\./,"")] = require(filePath + "/index");
-				}
+var appEngine = {
+	apps:[],
+	appsToLoad: 0,
+	appsLoaded: false,
+	nop: function(){},
+	executeAppsLoadedCallback: function(){
+		(this.AppsLoadedCallback || this.nop)(this.createExpressApp());
+	},
+	appLoaded: function(){
+		this.appsToLoad--;
+		if(this.appsToLoad<=0){
+			this.appsLoaded = true;
+			this.executeAppsLoadedCallback();
+		}
+	},
+	onAppsLoaded: function(callback){
+		this.AppsLoadedCallback = callback;
+		if(this.appsLoaded){
+			this.AppsLoadedCallback(this.createExpressApp());
+		}
+	},
+	startLoading: function (){
+		var fs = require('fs');
+		var path = "./apps/";
+		var that = this;
+		
+		fs.readdir(path,function(){
+			var files = arguments[1];
+			that.appsToLoad = files.length;
+			for(var idx in files){
+				fs.stat(path + files[idx], function(filePath){
+					return function(err, stat){
+						if(stat.isDirectory()){
+							that.apps[filePath.replace(/\./,"")] = require(filePath + "/index");
+						}
+						that.appLoaded();
+					}
+				}(path + files[idx]));
 			}
-		}(path + files[idx]));
-	}
-});
+		});
+	},
+	createExpressApp:function (){
+		var app = express();
 
-}
+		app.set('env', process.env.APP_ENGINE_ENV);
+		app.set('views', path.join(__dirname, 'views'));
+		app.set('view engine', 'jade');
 
-function getApplication(){
-	// view engine setup
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
+		app.use(favicon());
+		app.use(logger('dev'));
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded());
+		app.use(cookieParser());
+		app.use(express.static(path.join(__dirname, 'public')));
 
-	app.use(favicon());
-	app.use(logger('dev'));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded());
-	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, 'public')));
+		for(var idx in this.apps){
+			app.use(this.apps[idx]);
+			console.info("Loading App -> " + idx);
+		}
+		
+		// catch 404 and forward to error handler
+		app.use(function(req, res, next) {
+			var err = new Error('Not Found');
+			err.status = 404;
+			next(err);
+		});
 
-	console.info("Loading Apps: ");
-	for(var idx in apps){
-		app.use(apps[idx]);
-		console.info(" -> " + idx);
-	}
-	
-	/// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
-
-	/// error handlers
-
-	// development error handler
-	// will print stacktrace
-	if (app.get('env') === 'development') {
+		// development will print stacktrace
+		// production error handler
+		// no stacktraces leaked to user
 		app.use(function(err, req, res, next) {
 			res.status(err.status || 500);
-			res.render('error', {
+			res.render('error',  {
 				message: err.message,
-				error: err
+				error: (app.get('env') === 'development')?err:{}
 			});
 		});
+		return app;
 	}
+};
 
-	// production error handler
-	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-	return app;
-}
-
-module.exports = {init:init, getApplication:getApplication};
+module.exports = appEngine;

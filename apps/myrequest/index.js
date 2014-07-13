e300202a175dcc645c7ce652009199d19cc88978
Charles Lam <Charles.Lam@gmx.at>
2014-07-13 19:14:49 +0200

    add allrequest route and renames lastquery

--- apps/myrequest/index.js
+++ apps/myrequest/index.js
@@ -6,7 +6,7 @@ var matchPath = "/" + path.slice(path.length-2).join("/");
 
 var pg = require('pg');
 var connectionString = require("../shared-config.js").connectionString;
-var DB_QUERY_SELECT = "SELECT request FROM logged_requests ORDER BY updatetime DESC LIMIT 1";
+var DB_QUERY_SELECT = "SELECT request FROM logged_requests ORDER BY updatetime DESC;";
 var DB_QUERY_INSERT = "INSERT INTO logged_requests (request) VALUES($1);";
 
 function logLastRequest(req, res, next, request){
@@ -20,7 +20,7 @@ function logLastRequest(req, res, next, request){
 		});
 	});
 }
-router.all(matchPath + "*/lastquery", function(req, res, next) {
+router.all(matchPath + "*/lastrequest", function(req, res, next) {
 	pg.connect(connectionString, function(err, client, done){
 		client.query(DB_QUERY_SELECT, function(err, result){
 			data = result.rows[0].request;
@@ -34,6 +34,27 @@ router.all(matchPath + "*/lastquery", function(req, res, next) {
 		});
 	});
 });
+
+router.all(matchPath + "*/allrequests", function(req, res, next) {
+	pg.connect(connectionString, function(err, client, done){
+		client.query(DB_QUERY_SELECT, function(err, result){
+
+			if(err == null) {
+				var message = "";
+				for(var idx = 0; idx < result.rows.length;idx++){
+					message += result.rows[idx] + ", ";
+				}
+				if((message.length>3)
+					message = message.substr(0, message.length-2);
+					
+				res.end("[" + message + "]");
+			}
+			else{
+				next(new Error("No query"));
+			}
+		});
+	});
+})
 
 router.all(matchPath + "*", function(req, res, next) {
 	logLastRequest(req, res, next, req.body)

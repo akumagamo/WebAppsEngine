4b29793e33e6954d7ccd7ed78acb68913e3b23c5
Charles Lam <Charles.Lam@gmx.at>
2014-07-13 09:40:59 +0200

    minor refactoring

--- apps/myresponse/index.js
+++ apps/myresponse/index.js
@@ -8,32 +8,31 @@ var pg = require('pg');
 var connectionString = "postgres://servicePostgres:postGres@localhost/nodetest";
 
 function logLastRequest(req, res, next, request){
-
 		
-pg.connection(connectionString, function(err, client, done){
-
-
-client,query("insert into logged_requests (request) values($1);", request,function(err, result){
+	pg.connection(connectionString, function(err, client, done){
+		client,query("insert into logged_requests (request) values($1);", request,function(err, result){
 			if(err != null)
 				next(err);
 			else{
 				res.end();
 			}
+		});
 	});
 }
 router.all(matchPath + "*/lastquery", function(req, res, next) {
 		
-pg.connect(connectionString,function(err, client, done){
+	pg.connect(connectionString,function(err, client, done){
 
-client.query("select request from logged_requests order by updatetime desc LIMIT 1",[],function(err, result){
-	data = result.rows[0].request;
+		client.query("select request from logged_requests order by updatetime desc LIMIT 1",[],function(err, result){
+			data = result.rows[0].request;
 
-	if(err == null && data!="") {
-			res.end(data);
-		}
-		else{
-			next(new Error("No query"));
-		}
+			if(err == null && data!="") {
+				res.end(data);
+			}
+			else{
+				next(new Error("No query"));
+			}
+		});
 	});
 });
 

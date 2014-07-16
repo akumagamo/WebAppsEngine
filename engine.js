b43d7a8cbe203ab0a2dd9cbbfa5fe27101c3330d
Charles Lam <charles.lam@gmx.at>
2014-07-16 19:56:01 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -71,7 +71,7 @@ var appEngine = {
 		
 		// catch 404 and forward to error handler
 		app.use(function(req, res, next) {
-			var err = new Error('Not Found');
+			var err = new Error('Not Found 1');
 			err.status = 404;
 			next(err);
 		});

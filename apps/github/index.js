0a9106456c87049ba4394e5aa28adcd8243e37ef
Charles Lam <Charles.Lam@gmx.at>
2014-07-19 21:04:57 +0200

    ..quick test...

--- apps/github/index.js
+++ apps/github/index.js
@@ -9,7 +9,7 @@ var viewBasePath = ".." + matchPath + "/views";
 
 router.get(matchPath + "?*", function(req, res, next) {
 	var repoName = req.params["repo"];
-	console.info(repoNames);
+	console.info(repoName);
 	if(repoName){
 		var branchInfo = config[repoName];
 		if(req.body.ref && req.body.ref.indexOf(branchInfo.name)>-1){

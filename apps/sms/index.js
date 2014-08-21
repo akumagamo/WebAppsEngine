75101caa35e1aba27a9ce06e8cca5f12ea1ec873
Charles Lam <Charles.Lam@gmx.at>
2014-08-21 17:32:07 +0200

    minor fixes

--- apps/sms/index.js
+++ apps/sms/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var messages = require('./routes/messages');
 
-var path  = __dirname.split("\\");
+var path  = __dirname.split("/");
 var matchPath = "/" + path.slice(path.length-2).join("/");
+var viewBasePath = ".." + matchPath + "/views";
 
-router.get('/messages', messages.list);
-router.get('/messages/:id', messages.single);
-router.post('/messages', messages.send);
-router.delete('/messages/:id', messages.delete);
+router.get(matchPath, function(req, res) {
+	res.render(viewBasePath + "/msg", {});
+});
+
+)
+router.get(matchPath + "/messages", messages.list);
+router.get(matchPath +"/messages/:id", messages.single);
+router.post(matchPath +"/messages", messages.send);
+router.delete(matchPath +"/messages/:id", messages.delete);
 
 module.exports = router;
 

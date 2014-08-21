2baff05e2f061895f40cf4df1eace81f10405b83
Charles Lam <charles.lam@gmx.at>
2014-08-21 17:06:06 +0200

    Update index.js

--- apps/sms/index.js
+++ apps/sms/index.js
@@ -5,10 +5,11 @@ var messages = require('./routes/messages');
 var path  = __dirname.split("\\");
 var matchPath = "/" + path.slice(path.length-2).join("/");
 
-router.get('/messages', messages.list);
-router.get('/messages/:id', messages.single);
-router.post('/messages', messages.send);
-router.delete('/messages/:id', messages.delete);
+router.get(matchPath + "/messages", messages.list);
+router.get(matchPath +"/messages/:id", messages.single);
+router.post(matchPath +"/messages", messages.send);
+router.delete(matchPath +"/messages/:id", messages.delete);
 
 module.exports = router;
+
 

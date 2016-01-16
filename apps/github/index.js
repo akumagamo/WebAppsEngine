3b7dbf59c7e0d29bed95c61c3a597925d19b62f1
Charles Lam <charles.lam@gmx.at>
2016-01-16 13:00:09 +0000

    minor error

--- apps/github/index.js
+++ apps/github/index.js
@@ -9,23 +9,23 @@ var viewBasePath = ".." + matchPath + "/views";
 
 router.post(matchPath + "/:repo", function(req, res, next) {
 	var repoName = req.params["repo"];
-	console.info(repoName);
+//	console.info(repoName);
 	if(repoName){
 		var branchInfo = config[repoName];
-console.info("0");
-console.info(req.body.ref);
-console.info("0.0");
-console.info(branchInfo);
-console.info(req.body.ref.indexOf(branchInfo.name));
+//console.info("0");
+//console.info(req.body.ref);
+//console.info("0.0");
+//console.info(branchInfo);
+//console.info(req.body.ref.indexOf(branchInfo.name));
 		if(req.body.ref && req.body.ref.indexOf(branchInfo.name)>-1){
-console.info("1");
+//console.info("1");
 			var forwardRequest = http.request(branchInfo.options, function(){});
-console.info("2");
+//console.info("2");
 			setTimeout(function(){
 				forwardRequest.end();
 				res.end("Y");
 			},500);
-console.info("3");
+//console.info("3");
 		}else{
 			res.end("N");
 		}
